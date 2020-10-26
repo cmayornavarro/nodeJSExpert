2f580249746fbaa6954bac8762eed34875dea265
cmayornavarro <cmayornavarro@gmail.com>
2020-10-26 08:57:33 +0100

    with forks

---
+++ withForks.js
@@ -0,0 +1,25 @@
+//no tested
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+const server = app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/',(req,res,next) => {
+	for ( var i = 0; i<2e6;i++){}
+		res.send('Server says hi!');
+});
+
+app.get('/slow',(req,res,next) => {
+	
+	const compute = fork(bigComputation.js);
+	compute.send('start');
+	compute.on('message',(result) =>{
+		console.log(result);
+		res.send('Result' + result);
+	});
+});
