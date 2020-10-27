483c4d7841ca3d97112d595c10e37eae2056911b
cmayornavarro <cmayornavarro@gmail.com>
2020-10-27 08:56:17 +0100

    first graphQL file

--- streams/write.js
+++ streams/write.js
@@ -7,3 +7,11 @@ readableStream.setEncoding('utf8');
 readableStream.on('data', function(chunk) {
     writableStream.write(chunk);
 });
+
+/*
+nd, .pipe() has backflow handling, something your code does not. 
+When you call res.write() it returns a boolean. If that boolean is true,
+ then the write buffer is full and you should not be calling res.write()
+  again until the drain event occurs. Note, your code does not do that. So,
+   .pipe() is more complete than what many people will typically write themselves.
+*
